8e366a6590f90a9f3633ecf0a544e7a30ee1d255
Paul ZANNOU <paul.zannou@kamgoko.tech>
2026-07-15 16:18:09 +0100

    chore: remove unused backend PaulBot module and associated configuration files

--- backend/paulbot/paulbot.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { PaulbotController } from "./paulbot.controller";
-import { PaulbotService } from "./paulbot.service";
-
-@Module({
-  controllers: [PaulbotController],
-  providers: [PaulbotService],
-})
-export class PaulbotModule {}
