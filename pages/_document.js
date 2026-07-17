b3711099a00483ae681bb7519633428ce8bd85fe
Paul ZANNOU <paul.zannou@kamgoko.tech>
2026-07-17 18:10:26 +0100

    feat: integrate Google Tag Manager and Google Analytics 4 tags

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,28 @@ export default function Document() {
   return (
     <Html lang="fr">
       <Head>
+        {/* Google Tag Manager — le plus haut possible dans <head> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T8C9B5P9');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-H5387SXY6D" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-H5387SXY6D');`,
+          }}
+        />
+        {/* End Google tag (gtag.js) */}
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#110068" />
         <link rel="icon" href="/favicon.ico" />
@@ -16,6 +38,16 @@ export default function Document() {
         <link rel="dns-prefetch" href="https://www.google.com" />
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) — juste après <body> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T8C9B5P9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
